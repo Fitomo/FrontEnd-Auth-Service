4ff9c977b8fbbbc0f1f9e03aaa4380f73b698629
Aaron Freidus <aaronf101@gmail.com>
2016-06-16 15:36:24 -0700

    Resolves #7

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,15 +1,12 @@
-var session = require('express-session');
-var LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const LocalStrategy = require('passport-local').Strategy;
 // var User = require('../models/UserModel.js');
 
-module.exports = function(app, express, passport) {
-
+module.exports = (app, express, passport) => {
   app.use(session({
-      name: 'fitomo',
-      secret: 'fitomo',
-    }))
-
+    name: 'fitomo',
+    secret: 'fitomo',
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
-
 };
--- server/config/initialize.js
+++ server/config/initialize.js
@@ -1,17 +1,15 @@
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var jade = require('jade').__express;
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const jade = require('jade').__express;
 
 
-module.exports = function(app, express) {
-
+module.exports = (app, express) => {
   app.use(morgan('dev'));
-  app.use(express.static(__dirname + './../../client'));
+  app.use(express.static(`${__dirname}'./../../client`));
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.engine('jade', jade);
   app.set('view engine', 'jade');
-  app.set('views', __dirname + './../views');
-
+  app.set('views', `${__dirname}'./../views`);
 };
