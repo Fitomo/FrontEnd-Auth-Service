25aa9f54cce85816736f13afe958b434664d7a95
Ben Roa <benihanas1@gmail.com>
2016-06-16 19:42:29 -0700

    Combine client and server route rendering and linted files

--- src/actions/sample.js
+++ src/actions/sample.js
@@ -1,12 +1,12 @@
 import * as actionTypes from '../constants/actionTypes';
 
-//tracks is an array of objects 
+// tracks is an array of objects
 
 export function sampleAction(payload) {
   return {
     type: actionTypes.SAMPLE_SET,
-    payload
+    payload,
   };
-};
+}
 
-//return an object with action type and payload
+// return an object with action type and payload
