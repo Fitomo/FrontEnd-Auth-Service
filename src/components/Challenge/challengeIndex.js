5453ae8f460f162da4d1303ba51c23baae291845
Sehoon Park <milkpatron@me.com>
2016-06-20 12:06:25 -0700

    Delete some comments

--- src/components/Challenge/challengeIndex.js
+++ src/components/Challenge/challengeIndex.js
@@ -13,13 +13,3 @@ function mapStateToProps(state) {
 
 export default connect(mapStateToProps)(Challenge);
 
-// TO DO * not in order
-// review RESTful
-// ajax request from action creator to the database
-// fetch all user data
-// create friends table
-// make a friend request button
-// make a challenge button 
-// store a list of challenges with unique ids
-// distribute xp points to challengers
-// display challenges in challengePresenter.js
