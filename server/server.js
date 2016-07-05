c4adb953d1eb375da06215050d082f462bd6d082
Aaron Freidus <aaronf101@gmail.com>
2016-07-05 15:19:02 -0700

    FIXES SERVER NOT EXPOSING CORRECT PORT
    DO NOT REVERT THIS

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.get('/*', (req, res) => {
   res.redirect('/');
 });
 
-http.listen(8080, 'localhost', () => {
+app.listen(8080, () => {
   console.log('(CORS-enabled) Listening on 8080...');
 });
 
