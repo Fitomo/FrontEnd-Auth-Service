43704320bd9febe7607ce68714d226ee8ce9a9af
Sehoon Park <milkpatron@me.com>
2016-07-06 18:09:05 -0700

    Add team pictures on about page

--- src/components/About/aboutPresenter.js
+++ src/components/About/aboutPresenter.js
@@ -1,4 +1,8 @@
 import React, { PropTypes } from 'react';
+import aaron from '../../../Team/aaron-freidus.jpg';
+import ben from '../../../Team/ben-roa.jpg';
+import chris from '../../../Team/chris-ng.jpg';
+import sehoon from '../../../Team/sehoon-park.jpg';
 
 const About = ({ aboutUs }) => (
   <section className={aboutUs}>
@@ -6,19 +10,23 @@ const About = ({ aboutUs }) => (
     <div>
       <div>
         <span>Aaron</span>
-        <img src="" alt="Aaron" />
+        <img src={aaron} alt="Aaron" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus similique minima doloremque eos reprehenderit culpa hic architecto a quis minus corporis, amet! Natus cum in explicabo doloribus fuga magnam possimus!</p>
       </div>
       <div>
         <span> Ben</span>
-        <img src="" alt="Ben" />
+        <img src={ben} alt="Ben" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur veritatis laboriosam debitis quo, architecto sed minus fugiat iusto vel repudiandae qui quasi quos porro vero illum in, labore fuga! Quod.</p>
       </div>
       <div>
         <span>Chris</span>
-        <img src="" alt="Chris" />
+        <img src={chris} alt="Chris" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum doloribus similique temporibus numquam quae possimus voluptas quidem neque voluptatibus, nulla porro eveniet veritatis, quo voluptatem at ut suscipit enim?</p>
       </div>
       <div>
         <span>Sehoon</span>
-        <img src="" alt="Sehoon" />
+        <img src={sehoon} alt="Sehoon" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, laboriosam? Earum numquam laudantium fuga rem sunt at officiis, distinctio labore qui. Ipsa assumenda neque voluptatibus, modi! Dolorum laborum illum, totam!</p>
       </div>
     </div>
   </section>
